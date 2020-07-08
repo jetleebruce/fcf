ed06c5db08f03199ad56a0faa4c2158c766b912c
Serge Modin <modinsv@yandex.ru>
2020-07-08 18:00:56 +0300

    Update App.js
    Added useState for input and getting changes in console for array of messages which have been input

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [input, setInput] = useState("");
+  const [messages, setMessages] = useState([]);
+  console.log(input);
+ console.log(messages);
+  const sendMessage = (event) => {
+    setMessages([...messages, input]);
+    setInput("");
+  };
   return (
     <div className='App'>
       <h1>Hey Modin</h1>
+      <input value={input} onChange={(event) => setInput(event.target.value)} />
+      <button onClick={sendMessage}>Send Message</button>
     </div>
   );
 }
