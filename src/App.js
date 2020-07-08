d76a047e03c9bf49c1e698709a04fbdc1c153d4a
Serge Modin <modinsv@yandex.ru>
2020-07-08 18:23:09 +0300

    Update App.js
    Dispaly messages from message useState array

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import "./App.css";
 
 function App() {
   const [input, setInput] = useState("");
-  const [messages, setMessages] = useState([]);
+  const [messages, setMessages] = useState(["hello", "whats up", "here i am"]);
   console.log(input);
- console.log(messages);
+  console.log(messages);
   const sendMessage = (event) => {
+    event.preventDefault();
+
     setMessages([...messages, input]);
     setInput("");
   };
@@ -15,6 +17,11 @@ function App() {
       <h1>Hey Modin</h1>
       <input value={input} onChange={(event) => setInput(event.target.value)} />
       <button onClick={sendMessage}>Send Message</button>
+      {
+        messages.map(message => (
+          <p>{message}</p>
+        ))
+      }
     </div>
   );
 }
